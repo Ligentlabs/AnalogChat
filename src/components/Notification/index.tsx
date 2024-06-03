dee4a33065f0dc51a7e59099affdb41d35163805
Adarsh Gupta <blueadarsh1@gmail.com>
2024-06-03 08:37:20 +0000

    push

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -37,7 +37,7 @@ const useStyles = createStyles(({ css, token, isDarkMode }) => ({
         ${rgba(token.colorBgContainer, 0)},
         ${token.colorBgContainer} ${isDarkMode ? '80' : '140'}px
       ),
-      url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 24 24'%3E%3Cg fill='${token.colorFillTertiary}' %3E %3Cpolygon fill-rule='evenodd' points='8 4 12 6 8 8 6 12 4 8 0 6 4 4 6 0 8 4'/%3E%3C/g%3E%3C/svg%3E");
+      url("");
   `,
 }));
 
